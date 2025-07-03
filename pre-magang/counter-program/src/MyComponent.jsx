c422f23b7f5b13267524d621da7dd783a450d7bf
hp <144525103+4hannm@users.noreply.github.com>
2025-07-03 20:11:08 +0800

    8. Counter-program

---
+++ pre-magang/counter-program/src/MyComponent.jsx
@@ -0,0 +1,33 @@
+import React, {useState} from "react"
+
+
+function MyComponent(){
+    const [name, setName] = useState("...");
+    const [age, setAge] = useState(0);
+    const [isEmployed, setisEmployed] = useState(false);
+
+    const updateName = () => {
+        setName("Farhan");
+    }
+
+     const incrementAge = () => {
+        setAge(age + 1);
+    }
+
+     const toggleEmployedStatus = () => {
+        setisEmployed(!isEmployed);
+    }
+
+    return( <div>
+        <p>Name: {name} </p>
+        <button onClick={updateName}>Set Name</button>
+
+        <p>age: {age} </p>
+        <button onClick={incrementAge}>Increment Age</button>
+
+        <p>Is Employed: {isEmployed ? "Yes" : "No"} </p>
+        <button onClick={toggleEmployedStatus}>Status</button>
+    </div>)
+}
+
+export default MyComponent
